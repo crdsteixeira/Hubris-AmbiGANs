4898d9c7a66dd5a491a130e5cb6f84e27b248462
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-10-08 22:28:15 +0100

    feat: add get_chest_xray
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/datasets/datasets.py
+++ src/datasets/datasets.py
@@ -1,4 +1,8 @@
 import torchvision
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from PIL import Image
+import io
 
 
 def get_mnist(dataroot, train=True):
@@ -33,3 +37,33 @@ def get_cifar10(dataroot, train=True):
                                            ]))
 
     return dataset
+
+def get_chest_xray(dataroot, train=True):
+    split = "train" if train else "test"
+    ds = load_dataset("keremberke/chest-xray-classification", name="full")[split]
+    transform = torchvision.transforms.Compose([
+                                                torchvision.transforms.ToTensor(),
+                                                torchvision.transforms.Normalize(
+                                                (0.5,), (0.5,)),
+                                                ])
+    class ChestXrayDataset(Dataset):
+        def __init__(self, hf_dataset, transform=None):
+            self.hf_dataset = hf_dataset
+            self.transform = transform
+        
+        def __len__(self):
+            return len(self.hf_dataset)
+        
+        def __getitem__(self, idx):
+            sample = self.hf_dataset[idx]
+            image = Image.open(io.BytesIO(sample['image']))
+        
+            if self.transform:
+                image = self.transform(image)
+            
+            label = sample['label']
+            return image, label
+    
+    return ChestXrayDataset(ds, transform=transform)
+
+
