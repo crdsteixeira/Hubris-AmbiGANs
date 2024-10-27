d8d555360caf63d3da0688a9f671f5f4811cc748
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-10-27 02:08:04 +0000

    refactor: refactored logging
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,27 +1,22 @@
-"""Logging configs"""
+"""Logging configs."""
 
 import logging
 
+
 def configure_logging(level: str = "INFO") -> None:
-    """Configure logging with a specified level.
+    """
+    Configure logging with a specified level.
 
     Args:
         level (str): The logging level, e.g., 'DEBUG', 'INFO', 'WARNING', 'ERROR'.
+
     """
-    level_dict = {
-        "DEBUG": logging.DEBUG,
-        "INFO": logging.INFO,
-        "WARNING": logging.WARNING,
-        "ERROR": logging.ERROR
-    }
+    level_dict = {"DEBUG": logging.DEBUG, "INFO": logging.INFO, "WARNING": logging.WARNING, "ERROR": logging.ERROR}
 
     logging_level = level_dict.get(level.upper(), logging.INFO)
 
     logging.basicConfig(
         level=logging_level,
-        format='%(levelname)s - %(asctime)s - %(name)s - %(message)s',
-        handlers=[
-            logging.FileHandler("app.log"),
-            logging.StreamHandler()  # Prints logs to the console
-        ]
+        format="%(levelname)s - %(asctime)s - %(name)s - %(message)s",
+        handlers=[logging.FileHandler("app.log"), logging.StreamHandler()],  # Prints logs to the console
     )
