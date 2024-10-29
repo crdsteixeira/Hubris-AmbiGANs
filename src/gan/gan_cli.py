bf74736177afcfa34df0fca6f430a69be3d90300
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-10-29 01:15:46 +0000

    fix: fix ci cd
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/gan/gan_cli.py
+++ src/gan/gan_cli.py
@@ -10,7 +10,7 @@ import torch
 from dotenv import load_dotenv
 from pydantic import ValidationError
 
-import wandb  # type: ignore[attr-defined]
+import wandb
 from src.classifier.classifier_cache import ClassifierCache
 from src.datasets.load import load_dataset
 from src.gan.construct_gan import construct_gan, construct_loss
