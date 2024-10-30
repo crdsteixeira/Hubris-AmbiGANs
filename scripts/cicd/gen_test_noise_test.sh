c7b0ac75429db1824612d850f9a154665976caa7
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-10-30 20:59:45 +0000

    fix: fix typo in dataroot for test noise script
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- scripts/cicd/gen_test_noise_test.sh
+++ scripts/cicd/gen_test_noise_test.sh
@@ -2,7 +2,7 @@
 
 # Run the gen_test_noise script
 poetry run python -m src.gen_test_noise \
-    --out-dir="$FILESDIR/out" \
+    --out-dir="$FILESDIR/data" \
     --seed=42 \
     --nz=10 \
     --z-dim=10
