906162f486d46bb82fe811694554deae65ae8d31
Catia Teixeira <catia.rds.teixeira@gmail.com>
2024-10-30 22:42:52 +0000

    fix: add FILESDIR to all dirs
    Signed-off-by: Catia Teixeira <catia.rds.teixeira@gmail.com>

--- src/utils/read_config.py
+++ src/utils/read_config.py
@@ -34,4 +34,10 @@ def read_main_config(path: str) -> ConfigMain:
     with open(path, encoding="utf-8") as file:
         config_data = yaml.safe_load(file)
 
+    # Add base paths using FILESDIR
+    files_dir = os.environ.get("FILESDIR", "")
+    for rel_path_key in ["out_dir", "data_dir", "fid_stats_path", "test_noise"]:
+        if rel_path_key in config_data and isinstance(config_data[rel_path_key], str):
+            config_data[rel_path_key] = os.path.join(files_dir, config_data[rel_path_key])
+
     return ConfigMain(**config_data)
